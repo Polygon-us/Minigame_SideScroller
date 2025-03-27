5d301ee77158f5456dcaf9af521e6b81983cb627
Juaner <juan.ramirez@polygon.us>
2025-03-27 07:47:59 -0500

    add: input actions and initial movement

---
+++ Assets/Scripts/Character/CharacterMovement.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine.InputSystem;
+using UnityEngine;
+using System;
+
+namespace Character
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(GroundChecker))]
+    public class CharacterMovement : MonoBehaviour
+    {
+        [SerializeField] private float acceleration;
+        [SerializeField] private float maxSpeed;
+        [SerializeField] private float jumpForce;
+        [SerializeField] private float gravityMultiplier;
+
+        private GroundChecker _groundChecker;
+        private Rigidbody2D _rb;
+        private MoveActions _moveActions;
+        
+        private void OnEnable()
+        {
+            _moveActions.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _moveActions.Disable();
+        }
+
+        private void OnDestroy()
+        {
+            _moveActions.Dispose();
+        }
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+            _groundChecker = GetComponent<GroundChecker>();
+            
+            _moveActions = new MoveActions();
+            _moveActions.Player.Jump.performed += OnJump;
+        }
+
+        private void Update()
+        {
+            if (_moveActions.Player.Jump.IsInProgress())
+                Move();
+        }
+
+        private void Move()
+        {
+            _rb.linearVelocityX += moveSpeed * _moveActions.Player.Move.ReadValue<Vector2>().x * Time.deltaTime;
+            _rb.linearVelocityX = MathF.Clamp(_rb.linearVelocityX, 0, maxSpeed);
+        }
+        
+        private void OnJump(InputAction.CallbackContext context)
+        {
+            if (_groundChecker.IsGrounded)
+                _rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+}
---
+++ Assets/Scripts/Character/GroundChecker.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Character
+{
+    public class GroundChecker : MonoBehaviour
+    {
+        [SerializeField] private Vector3 position;
+        [SerializeField] private float checkRadius;
+        [SerializeField] private LayerMask groundLayer;
+        
+        private bool _isGrounded;
+        
+        public bool IsGrounded => _isGrounded;
+
+        private void FixedUpdate()
+        {
+            _isGrounded = Physics2D.CircleCast( transform.position + position, checkRadius, Vector2.down, checkRadius, groundLayer);
+        }
+
+#if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = _isGrounded ? Color.red : Color.green;
+            Gizmos.DrawWireSphere(transform.position + position, checkRadius);
+        }
+#endif
+    }
+}
